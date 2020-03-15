5f473123ce422b05005130915c8715a655885198
varsha verma <varsha.verma85@gmail.com>
2020-03-15 11:03:10 +0100

    created pie chart json

--- dashapp/models.py
+++ dashapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_id = models.CharField(max_length=50)
+    product_name = models.CharField(max_length=50)
+    def __str__(self):
+        return "{}".format(self.product_name)
+
+class Release(models.Model):
+    release_id = models.IntegerField()
+    release_name = models.CharField(max_length=50)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    
+    
+class Testcase(models.Model):
+    test_id = models.IntegerField()
+    title = models.CharField(max_length=50) 
+    
+    def __str__(self):
+        return "{}".format(self.title)
+            
+class Testrun(models.Model):
+    test_id = models.ForeignKey(Testcase, on_delete=models.CASCADE)
+    release_id = models.ForeignKey(Release, on_delete=models.CASCADE)
+    test_result = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "{}-{}".format(self.test_id.title, self.test_result)  
+
+    
+    
